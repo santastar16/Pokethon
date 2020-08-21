49e5b2f44d9c8107e9116bd4d71e4f30f447e66a
santastar16 <67168702+santastar16@users.noreply.github.com>
2020-08-20 23:54:41 -0700

    First commit submission of Pokethon Project

---
+++ script.js
@@ -0,0 +1,29 @@
+function selectPikachu(){
+  pikachuCard = document.getElementById("pikachu-pokemon-card");
+  pikachuCard.style.opacity = 1.0;
+  alert("PIKA PIKA");
+};
+
+function selectCharizard(){
+  charizardCard = document.getElementById("charizard-pokemon-card");
+  charizardCard.style.opacity = 1.0;
+  alert("CHARIZARD I CHOOSE YOU");
+};
+
+function selectLucario(){
+  lucarioCard = document.getElementById("lucario-pokemon-card");
+  lucarioCard.style.opacity = 1.0;
+  alert("LUCARIO GO");
+};
+
+function selectBlastoise(){
+  blastoiseCard = document.getElementById("blastoise-pokemon-card");
+  blastoiseCard.style.opacity = 1.0;
+  alert("BLASSSSSTTTOIIISEEE");
+};
+
+function selectBulbasaur(){
+  bulbasaurCard = document.getElementById("bulbasaur-pokemon-card");
+  bulbasaurCard.style.opacity = 1.0;
+  alert("Bulba - SAUR");
+};
